9cbeaab86dff5aa45d3b03c2446333ab84bf546e
Rafael Alba <ralbas.int@gmail.com>
2026-07-20 18:47:06 +0200

    Load compact opportunity workspace styles

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 
 import { Workbench } from "./Workbench";
 import "./styles.css";
+import "./CompactOpportunityWorkspace.css";
 
 const rootElement = document.getElementById("root");
 
