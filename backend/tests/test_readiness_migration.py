f8579b318d15c3664ef0a3e9a33dade7728757ab
Rafael Alba <ralbas.int@gmail.com>
2026-07-24 20:14:56 +0200

    Advance readiness migration head revision

--- backend/tests/test_readiness_migration.py
+++ backend/tests/test_readiness_migration.py
@@ -8,7 +8,7 @@ from alembic.config import Config
 from sqlalchemy import create_engine, inspect
 
 PREVIOUS_REVISION = "20260712_0003"
-CURRENT_REVISION = "20260724_0008"
+CURRENT_REVISION = "20260724_0009"
 TABLE_NAME = "application_readiness_reports"
 INDEX_NAME = "ix_application_readiness_reports_posting_id"
 
