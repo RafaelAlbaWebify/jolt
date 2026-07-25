30bca6a1cc050b256468313271cd2c23ec5bc7c7
Rafael Alba <ralbas.int@gmail.com>
2026-07-25 10:44:01 +0200

    Load structured extraction styles

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -12,6 +12,7 @@ import "./ApplicationWorkItems.css";
 import "./OpportunityApplicationHandoff.css";
 import "./MarketIntelligence.css";
 import "./ProfessionalIntelligence.css";
+import "./ProfessionalStructuredExtraction.css";
 import "./WorkflowRefinement.css";
 
 const rootElement = document.getElementById("root");
